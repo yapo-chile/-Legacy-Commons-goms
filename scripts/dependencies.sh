899e5635bd1be9e317d6dedad1e9d4d158fa26f9
Miguel Diaz <miguel@schibsted.cl>
2018-01-17 15:14:59 -0300

    test-cover - Implement gometalinter

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -4,9 +4,6 @@ set -e
 
  # Probably there's a better way to fetch our dependencies
 
-go get github.com/golang/lint/golint                # Linter
-go get honnef.co/go/tools/cmd/megacheck             # Badass static analyzer/linter
-go get github.com/fzipp/gocyclo                     # Function cyclomatic complexity analyzer
 go get github.com/axw/gocov                         # Coverage reporting tool
 go get github.com/AlekSi/gocov-xml                  # Generate XML output in Cobertura format
 go get github.com/jstemmer/go-junit-report          # Converts go test output to an xml repor
