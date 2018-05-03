863b529a5e0462d1b26c0dc35997a2a469fc8a89
Miguel Diaz <miguel@schibsted.cl>
2018-05-03 16:00:56 -0300

    port-new-features - Test logger usage on fibonacci usecases

--- pkg/usecases/fibonacci_test.go
+++ pkg/usecases/fibonacci_test.go
@@ -25,22 +25,40 @@ func (m MockFibonacciRepository) LatestPair() domain.FibonacciPair {
 	return ret.Get(0).(domain.FibonacciPair)
 }
 
+type MockFibonacciLogger struct {
+	mock.Mock
+}
+
+func (m MockFibonacciLogger) LogBadInput(x int) {
+	m.Called(x)
+}
+func (m MockFibonacciLogger) LogRepositoryError(i int, x domain.Fibonacci, err error) {
+	m.Called(i, x, err)
+}
+
 func TestFibonacciInteractorGetNthNegative(t *testing.T) {
+	l := &MockFibonacciLogger{}
 	m := MockFibonacciRepository{}
 	i := FibonacciInteractor{
+		Logger:     l,
 		Repository: m,
 	}
+
+	l.On("LogBadInput", -1)
 
 	_, err := i.GetNth(-1)
 	assert.Error(t, err)
 	m.AssertExpectations(t)
+	l.AssertExpectations(t)
 }
 
 func TestFibonacciInteractorGetNthKnown(t *testing.T) {
+	l := &MockFibonacciLogger{}
 	m := MockFibonacciRepository{}
 	m.On("Get", 1).Return(domain.Fibonacci(1), nil)
 
 	i := FibonacciInteractor{
+		Logger:     l,
 		Repository: m,
 	}
 
@@ -48,9 +66,11 @@ func TestFibonacciInteractorGetNthKnown(t *testing.T) {
 	assert.Equal(t, domain.Fibonacci(1), x)
 	assert.NoError(t, err)
 	m.AssertExpectations(t)
+	l.AssertExpectations(t)
 }
 
 func TestFibonacciInteractorGetNthUnknown(t *testing.T) {
+	l := &MockFibonacciLogger{}
 	m := MockFibonacciRepository{}
 	m.On("Get", 4).Return(domain.Fibonacci(-1), errors.New("Some error")).Once()
 	m.On("LatestPair").Return(domain.FibonacciPair{IA: 1, A: domain.Fibonacci(1), IB: 2, B: domain.Fibonacci(1)}).Once()
@@ -63,6 +83,7 @@ func TestFibonacciInteractorGetNthUnknown(t *testing.T) {
 	m.On("Get", 4).Return(domain.Fibonacci(3), nil)
 
 	i := FibonacciInteractor{
+		Logger:     l,
 		Repository: m,
 	}
 
@@ -70,19 +91,24 @@ func TestFibonacciInteractorGetNthUnknown(t *testing.T) {
 	assert.Equal(t, domain.Fibonacci(3), x)
 	assert.NoError(t, err)
 	m.AssertExpectations(t)
+	l.AssertExpectations(t)
 }
 
 func TestFibonacciInteractorGetNthSaveError(t *testing.T) {
+	l := &MockFibonacciLogger{}
 	m := MockFibonacciRepository{}
 	m.On("Get", 4).Return(domain.Fibonacci(-1), errors.New("Some error")).Once()
 	m.On("LatestPair").Return(domain.FibonacciPair{IA: 1, A: domain.Fibonacci(1), IB: 2, B: domain.Fibonacci(1)}).Once()
 	m.On("Save", 3, domain.Fibonacci(2)).Return(errors.New("Weird error")).Once()
 
+	l.On("LogRepositoryError", 3, domain.Fibonacci(2), errors.New("Weird error"))
 	i := FibonacciInteractor{
+		Logger:     l,
 		Repository: m,
 	}
 
 	_, err := i.GetNth(4)
 	assert.Error(t, err)
 	m.AssertExpectations(t)
+	l.AssertExpectations(t)
 }
