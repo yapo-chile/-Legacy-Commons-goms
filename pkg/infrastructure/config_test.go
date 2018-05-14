5b70c58b8099f33b874e48996c8006d592f4c7f2
Dany Jaque <djaque@gmail.com>
2018-05-14 12:44:57 -0400

    secrets - improve config tests

--- pkg/infrastructure/config_test.go
+++ pkg/infrastructure/config_test.go
@@ -1,6 +1,8 @@
 package infrastructure
 
 import (
+	"io/ioutil"
+	"log"
 	"os"
 	"testing"
 
@@ -12,17 +14,39 @@ type Nested struct {
 }
 
 type TestConf struct {
-	I int    `env:"LE_I"`
-	S string `env:"LE_S"`
-	F string `env:"FROM_FILE"`
-	N Nested `env:"NESTED_"`
+	I  int    `env:"LE_I"`
+	S  string `env:"LE_S"`
+	F  string `env:"FROM"`
+	N  Nested `env:"NESTED_"`
+	D  string `env:"DEF" envDefault:"default_conf"`
+	OF string `env:"OTHERFILE"`
+}
+
+func createFile(prefix string) string {
+	content := []byte("temp_data")
+	tmpfile, err := ioutil.TempFile("", prefix)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := tmpfile.Write(content); err != nil {
+		log.Fatal(err)
+	}
+	if err := tmpfile.Close(); err != nil {
+		log.Fatal(err)
+	}
+	return tmpfile.Name() // remember clean up
 }
 
 func TestConfigLoad(t *testing.T) {
+	dummyFile := createFile("lala")
+	defer os.Remove(dummyFile)
 	env := map[string]string{
-		"LE_I":        "42",
-		"LE_S":        "Don't panic",
-		"NESTED_LE_F": "true",
+		"LE_I":           "42",
+		"LE_S":           "Don't panic",
+		"NESTED_LE_F":    "true",
+		"FROM_FILE":      dummyFile,
+		"OTHERFILE_FILE": "/fileMissing.txt",
 	}
 	// Setup environment
 	for k, v := range env {
@@ -36,9 +60,11 @@ func TestConfigLoad(t *testing.T) {
 	expected := TestConf{
 		I: 42,
 		S: "Don't panic",
+		F: "temp_data",
 		N: Nested{
 			F: true,
 		},
+		D: "default_conf",
 	}
 
 	assert.Equal(t, expected, conf)
