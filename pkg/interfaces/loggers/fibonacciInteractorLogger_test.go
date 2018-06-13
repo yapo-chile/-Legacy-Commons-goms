e6fec3797ff680802196bf6bcaf030395f605f5e
Miguel Diaz <miguel@schibsted.cl>
2018-06-13 15:21:49 -0400

    add-race-detector - Add test coverage for loggers

---
+++ pkg/interfaces/loggers/fibonacciInteractorLogger_test.go
@@ -0,0 +1,27 @@
+package loggers
+
+import (
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/Yapo/goutils"
+)
+
+// There are no return values to assert on, as logger only cause side effects
+// to communicate with the outside world. These tests only ensure that the
+// loggers don't panic
+
+func TestFibonacciInteractorDefaultLogger(t *testing.T) {
+	l := MakeFibonacciInteractorLogger(loggerMock{t: t})
+	l.LogBadInput(42)
+	l.LogRepositoryError(5, 42, nil)
+}
+
+func TestJSONHandlerLogger(t *testing.T) {
+	r := httptest.NewRequest("GET", "/test", strings.NewReader(""))
+	l := MakeJSONHandlerLogger(loggerMock{t: t})
+	l.LogRequestStart(r)
+	l.LogRequestEnd(r, &goutils.Response{})
+	l.LogRequestPanic(r, &goutils.Response{}, nil)
+}
