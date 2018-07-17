20c76578bacef545c12fb4fe2fae4762c4930f2a
mauro <mauro.delucca@schibsted.com>
2018-07-17 16:47:02 -0400

    Restricting docker start to local environment

--- scripts/commands/docker-build.sh
+++ scripts/commands/docker-build.sh
@@ -38,15 +38,18 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -o ${DOCKER_BINARY} ./${MAIN_F
 
 set +e
 
-echoTitle "Starting Docker Engine"
-if [[ $OSTYPE == "darwin"* ]]; then
-    echoTitle "Starting Mac OSX Docker Daemon"
-    $DIR/docker-start-macosx.sh
-elif [[ "$OSTYPE" == "linux-gnu" ]]; then
-    echoTitle "Starting Linux Docker Daemon"
-    sudo start-docker-daemon
-else
-    echoError "Platform not supported"
+#In case it is not Travis we make sure docker is running
+if ! [[ -n "$TRAVIS" ]]; then
+    echoTitle "Starting Docker Engine"
+    if [[ $OSTYPE == "darwin"* ]]; then
+        echoTitle "Starting Mac OSX Docker Daemon"
+        $DIR/docker-start-macosx.sh
+    elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+        echoTitle "Starting Linux Docker Daemon"
+        sudo start-docker-daemon
+    else
+        echoError "Platform not supported"
+    fi
 fi
 
 echoTitle "Building docker image for ${DOCKER_IMAGE}"
