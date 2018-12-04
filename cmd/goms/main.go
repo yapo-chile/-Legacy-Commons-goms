583a1e83df5bcbe4e6c95968ff23453ee15bac11
Miguel Diaz <miguel@schibsted.cl>
2018-12-04 12:44:00 -0300

    little-tweaks - Fix conf logging and remove redundant web server start

--- cmd/goms/main.go
+++ cmd/goms/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"net/http"
 	"os"
 
 	"github.schibsted.io/Yapo/goms/pkg/infrastructure"
@@ -19,7 +18,7 @@ func main() {
 	var conf infrastructure.Config
 	shutdownSequence.Listen()
 	infrastructure.LoadFromEnv(&conf)
-	if jconf, err := json.MarshalIndent(conf, "", "    "); err != nil {
+	if jconf, err := json.MarshalIndent(conf, "", "    "); err == nil {
 		fmt.Printf("Config: \n%s\n", jconf)
 	}
 
@@ -90,9 +89,8 @@ func main() {
 		logger,
 	)
 	shutdownSequence.Push(server)
+	logger.Info("Starting request serving")
 	go server.ListenAndServe()
 	shutdownSequence.Wait()
-
-	logger.Info("Starting request serving")
-	logger.Crit("%s\n", http.ListenAndServe(conf.ServiceConf.Host, maker.NewRouter()))
+	logger.Info("Server exited normally")
 }
