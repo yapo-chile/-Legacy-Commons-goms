4f49265a81cea44c86fe59d565e902e67d15effe
Piero Valdebenito <piero.valdebenito@schibsted.com>
2019-03-15 12:49:24 -0300

    [fix/prometheus-custom-metrics] - Fix unit tests for new custom metrics

--- pkg/interfaces/loggers/fibonacciPrometheusLogger_test.go
+++ pkg/interfaces/loggers/fibonacciPrometheusLogger_test.go
@@ -14,22 +14,56 @@ type prometheusMock struct {
 	mock.Mock
 }
 
+func (m *prometheusMock) NewCounterVector(name, help string, labels []string) CounterVector {
+	args := m.Called(name, help, labels)
+	return args.Get(0).(CounterVector)
+}
+
 func (m *prometheusMock) Close() error {
 	args := m.Called()
 	return args.Error(0)
 }
 
-func (m *prometheusMock) IncrementCounter(metric MetricType) {
-	m.Called(metric)
+type mockCollector struct {
+	mock.Mock
+}
+
+func (m *mockCollector) WithLabelValues(labels ...string) Counter {
+	args := m.Called(labels)
+	return args.Get(0).(Counter)
+}
+
+type mockCounter struct {
+	mock.Mock
+}
+
+func (m *mockCounter) Inc() {
+	m.Called()
+}
+
+func (m *mockCounter) Add(value float64) {
+	m.Called(value)
 }
 
 func TestFibonacciInteractorDefaultLogger(t *testing.T) {
 	m := &loggerMock{t: t}
 	mPrometheus := &prometheusMock{}
-	mPrometheus.On("IncrementCounter", BadInputError).Once()
-	mPrometheus.On("IncrementCounter", RepositoryError).Once()
+	mCollector := &mockCollector{}
+	mCounter := &mockCounter{}
+	mPrometheus.On("NewCounterVector",
+		mock.AnythingOfType("string"),
+		mock.AnythingOfType("string"),
+		mock.AnythingOfType("[]string")).Return(mCollector).Once()
+
 	l := MakeFibonacciPrometheusLogger(m, mPrometheus)
+
+	mCollector.On("WithLabelValues", mock.AnythingOfType("[]string")).Return(mCounter)
+	mCounter.On("Inc")
+
 	l.LogBadInput(42)
 	l.LogRepositoryError(5, 42, nil)
 	m.AssertExpectations(t)
+	mPrometheus.AssertExpectations(t)
+	mCollector.AssertExpectations(t)
+	mCounter.AssertExpectations(t)
 }
