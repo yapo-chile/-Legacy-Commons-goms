cf1dacac0cae354c580ed1ac0cbfcdde8124b8fe
Piero Valdebenito <piero.valdebenito@schibsted.com>
2019-04-01 12:25:30 -0300

    [feat/auto-export-log-to-prometheus] - Fix comments

--- pkg/infrastructure/yapoLogger.go
+++ pkg/infrastructure/yapoLogger.go
@@ -37,40 +37,40 @@ func (y *yapoLogger) init(config *LoggerConf) error {
 	return nil
 }
 
-// LogDebug logs a message at DEBUG level
+// Debug logs a message at DEBUG level
 func (y yapoLogger) Debug(format string, params ...interface{}) {
 	logger.Debug(format, params...)
 }
 
-// LogInfo logs a message at INFO level.
+// Info logs a message at INFO level.
 // Info events are automatically exported to prometheus.
 func (y yapoLogger) Info(format string, params ...interface{}) {
 	y.metrics.CollectEvent(getEntityName(), getEventName(), getEventType())
 	logger.Info(format, params...)
 }
 
-// LogSuccess logs a message as Success event.
+// Success logs a message as Success event.
 // Success events are automatically exported to prometheus.
 func (y yapoLogger) Success(format string, params ...interface{}) {
 	y.metrics.CollectEvent(getEntityName(), getEventName(), getEventType())
 	logger.Info(format, params...)
 }
 
-// LogWarn logs a message at WARNING level
+// Warn logs a message at WARNING level.
 // warning events are automatically exported to prometheus.
 func (y yapoLogger) Warn(format string, params ...interface{}) {
 	y.metrics.CollectEvent(getEntityName(), getEventName(), getEventType())
 	logger.Warn(format, params...)
 }
 
-// LogError logs a message at ERROR level
+// Error logs a message at ERROR level.
 // Error events are automatically exported to prometheus.
 func (y yapoLogger) Error(format string, params ...interface{}) {
 	y.metrics.CollectEvent(getEntityName(), getEventName(), getEventType())
 	logger.Error(format, params...)
 }
 
-// LogCrit logs a message at CRITICAL level
+// LogCrit logs a message at CRITICAL level.
 // Critical events are automatically exported to prometheus.
 func (y yapoLogger) Crit(format string, params ...interface{}) {
 	y.metrics.CollectEvent(getEntityName(), getEventName(), getEventType())
