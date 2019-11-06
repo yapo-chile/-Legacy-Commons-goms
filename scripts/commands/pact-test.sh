033dfdd27505f17b10be6a1a2d4bf997d27c7b70
Mauricio Galeas <mauro.gm.92@gmail.com>
2019-11-06 18:11:46 -0300

    [feat/pactUpdate] Pact tests config update

--- scripts/commands/pact-test.sh
+++ scripts/commands/pact-test.sh
@@ -2,11 +2,6 @@
 export PACT_TEST_ENABLED=true
 export PACT_DIRECTORY=./pact
 export SEARCH_MS_PORT=8089
-export AD_API_PATH=http://localhost:${SEARCH_MS_PORT}/api/v1
-export AD_GET_INFO=${AD_API_PATH}/search/list_id
-export TOUCHSTONE_PORT=8088
-export TOUCHSTONE_API_PATH=http://localhost:${TOUCHSTONE_PORT}
-export TOUCHSTONE_GET_INFO=${TOUCHSTONE_API_PATH}/suggestions/marketplaces/yapocl/ads/
 export SERVICE_HOST=:8090
 
 file=pact-go_$(uname -s)_amd64.tar.gz
@@ -28,14 +23,6 @@ echoTitle "Starting pact-go daemon in background"
 nohup pact/bin/pact-go daemon > daemon.out 2> daemon.err &
 PACT_PID=$!
 
-: ' echoTitle "Starting search-ms mock in background"
-nohup pact/bin/pact/bin/pact-stub-service pact/mocks/search-ms.json --port=${SEARCH_MS_PORT} &
-SEARCH_MS_PID=$!
-
-echoTitle "Starting touchstone-api mock in background"
-nohup pact/bin/pact/bin/pact-stub-service pact/mocks/touchstone.json --port=${TOUCHSTONE_PORT} &
-TOUCHSTONE_PID=$! '
-
 echoTitle "Starting ${PACT_BINARY} in background"
 nohup ./${PACT_BINARY} > ${PACT_BINARY}.out 2> ${PACT_BINARY}.err &
 MS_PID=$!
@@ -45,6 +32,6 @@ cd pact
 go test -v -run TestProvider
 
 echoTitle "Killing daemons"
-kill ${PACT_PID} ${SEARCH_MS_PID} ${TOUCHSTONE_PID} ${MS_PID}
+kill ${PACT_PID} ${MS_PID}
 
 echoTitle "Done"
