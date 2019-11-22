77b49cdce8777329ac607db6ffc63bfaa94b2633
Mauricio Galeas <mauro.gm.92@gmail.com>
2019-11-22 11:45:31 -0300

    [feat/brokerCond] Old version of pact retrieving from broker

--- pact/provider_test.go
+++ pact/provider_test.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 	"testing"
 
 	"github.com/pact-foundation/pact-go/dsl"
@@ -23,6 +24,18 @@ type PactConf struct {
 	ProviderHost string `env:"PROVIDER_HOST" envDefault:"http://localhost"`
 	ProviderPort string `env:"PROVIDER_PORT" envDefault:"8080"`
 }
+type Detail struct {
+	Title string `json:"title"`
+	Name  string `json:"name"`
+	Href  string `json:"href"`
+}
+type ConsumerVersion struct {
+	Details Detail `json:"pb:consumer-version"`
+}
+type JSONTemp struct {
+	Links        ConsumerVersion `json:"_links"`
+	Interactions []interface{}   `json:"interactions"`
+}
 
 // Example Provider Pact: How to run me!
 // 1. Start the daemon with `./pact-go daemon`
@@ -35,7 +48,6 @@ func TestProvider(t *testing.T) {
 	var pact = &dsl.Pact{
 		Consumer: "goms",
 		Provider: "profile-ms",
-		Port:     6666,
 	}
 	files, err := IOReadDir(pactDir)
 	if err != nil {
@@ -59,41 +71,31 @@ func TestProvider(t *testing.T) {
 func TestSendBroker(t *testing.T) {
 	pactPublisher := &dsl.Publisher{}
 	var conf PactConf
-	var conf2 infrastructure.Config
-	type JSONTemp struct {
-		Provider string
-	}
-	var contractJSON JSONTemp
+	newVer := 0.0
+	//var contractJSON JSONTemp
 
 	infrastructure.LoadFromEnv(&conf)
-	infrastructure.LoadFromEnv(&conf)
-	prometheus := infrastructure.MakePrometheusExporter(
-		conf2.PrometheusConf.Port,
-		conf2.PrometheusConf.Enabled,
-	)
-	logger, err := infrastructure.MakeYapoLogger(&conf2.LoggerConf,
-		prometheus.NewEventsCollector(
-			"goms_service_events_total",
-			"events tracker counter for goms service",
-		),
-	)
-	HTTPHandler := infrastructure.NewHTTPHandler(logger)
-	httprequest := HTTPHandler.NewRequest().
-		SetMethod("GET").
-		SetPath("http://3.229.36.112/pacts/provider/profile-ms/consumer/goms/latest")
-	publishedContract, err := HTTPHandler.Send(httprequest)
-	resp := fmt.Sprintf("%s", publishedContract)
-	err = json.Unmarshal([]byte(resp), &contractJSON)
-	fmt.Printf("Request preview %+v\n", contractJSON.Provider)
+
+	oldPactResponse, currentVer, err := getContractInfo(conf.BrokerHost +
+		"/pacts/provider/profile-ms/consumer/goms/latest")
 
 	if err != nil {
-		fmt.Printf("Error with request preview %+v\n", err)
+		fmt.Printf("Error getting old contract %+v\n", err)
 	}
+	newPactResponse, err := getJSONPactFile(pactDir)
+	if err != nil {
+		fmt.Printf("Error getting pact response to send %+v\n", err)
+	}
+	if oldPactResponse != nil {
+		newVer = currentVer + 0.1
+	}
+	fmt.Printf("Old pact %+v\n", oldPactResponse)
+	fmt.Printf("New pact %+v\n", newPactResponse)
 
 	err = pactPublisher.Publish(types.PublishRequest{
 		PactURLs:        []string{"./pacts/goms.json"},
 		PactBroker:      conf.BrokerHost + ":" + conf.BrokerPort,
-		ConsumerVersion: "2.0.0",
+		ConsumerVersion: fmt.Sprintf("%.1f", newVer),
 		Tags:            []string{"goms"},
 	})
 	if err != nil {
@@ -118,4 +120,62 @@ func IOReadDir(root string) ([]string, error) {
 		files = append(files, file.Name())
 	}
 	return files, nil
+}
+func getContractInfo(url string) (interface{}, float64, error) {
+
+	var conf2 infrastructure.Config
+
+	infrastructure.LoadFromEnv(&conf2)
+	prometheus := infrastructure.MakePrometheusExporter(
+		conf2.PrometheusConf.Port,
+		conf2.PrometheusConf.Enabled,
+	)
+	logger, _ := infrastructure.MakeYapoLogger(&conf2.LoggerConf,
+		prometheus.NewEventsCollector(
+			"goms_service_events_total",
+			"events tracker counter for goms service",
+		),
+	)
+
+	headers := make(map[string]string)
+	headers["Content-Type"] = "application/json"
+
+	HTTPHandler := infrastructure.NewHTTPHandler(logger)
+	httprequest := HTTPHandler.NewRequest().
+		SetMethod("GET").
+		SetPath(url)
+	publishedContract, err := HTTPHandler.Send(httprequest)
+	if err != nil {
+
+		return nil, -1, err
+	}
+	resp := fmt.Sprintf("%s", publishedContract)
+
+	var result JSONTemp
+	json.Unmarshal([]byte(resp), &result)
+
+	pactRsponse := result.Interactions[1].(map[string]interface{})["response"]
+
+	versionFloat, err := strconv.ParseFloat(result.Links.Details.Name, 64)
+	if err != nil {
+
+		return nil, -1, err
+	}
+	return pactRsponse, versionFloat, nil
+
+}
+func getJSONPactFile(pactDir string) (interface{}, error) {
+	var result JSONTemp
+	file, err := IOReadDir(pactDir)
+	if err != nil {
+		fmt.Printf("Error in reading files. Error %+v", err)
+	}
+	pactFileToSend, err := ioutil.ReadFile(pactDir + "/" + file[0])
+	if err != nil {
+		return nil, err
+	}
+	resp := fmt.Sprintf("%s", pactFileToSend)
+	json.Unmarshal([]byte(resp), &result)
+	pactResponse := result.Interactions[1].(map[string]interface{})["response"]
+	return pactResponse, err
 }
