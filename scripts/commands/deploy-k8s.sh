8a1149eed269a044b3fc334f147c5905a2824411
Ana Mora <ana.mora@schibsted.cl>
2019-12-23 15:19:25 -0300

    k8s Add repo link to Chart.yaml

--- scripts/commands/deploy-k8s.sh
+++ scripts/commands/deploy-k8s.sh
@@ -3,14 +3,6 @@ echo "Publishing helm package to Artifactory"
 export CHART_DIR=k8s/goms
 export CHART_VERSION=$(grep version $CHART_DIR/Chart.yaml | awk '{print $2}')
 
-helm fetch "${ARTIFACTORY_CONTEXT}/helm-virtual/yapo/goms" --version ${CHART_VERSION} 2> /dev/null
-if [ $? -eq 0 ]; then
-    echo "The Chart is already exists in Artifactory with this version"
-else
-    helm lint ${CHART_DIR}
-	helm package ${CHART_DIR} --version ${CHART_VERSION}
-	jfrog rt u "*.tgz" "helm-local/yapo/"
-fi
 helm lint ${CHART_DIR}
 helm package ${CHART_DIR} --version ${CHART_VERSION}
 jfrog rt u "*.tgz" "helm-local/yapo/" || true
