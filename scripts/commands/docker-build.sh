50b07c8ae680f33f782332b101307b9ab1f40dbb
Miguel Diaz <miguel.diaz@adevinta.com>
2020-01-20 12:42:56 -0300

    simpler-build - Eliminate deploy vars from vars.mk

--- scripts/commands/docker-build.sh
+++ scripts/commands/docker-build.sh
@@ -7,19 +7,6 @@ if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 
 ########### DYNAMIC VARS ###############
 
-#In case we are in travis, docker tag will be "branch_name-20180101-1200". In case of master branch, branch_name is blank.
-#In case of local build (not in travis) tag will be "local".
-
-if [[ -n "$TRAVIS" ]]; then
-    if [ "${GIT_BRANCH}" != "master" ]; then
-        DOCKER_TAG=$(echo ${GIT_BRANCH} | tr '[:upper:]' '[:lower:]' | sed 's,/,_,g')
-    else
-        DOCKER_TAG=$(TZ=UTC git show --quiet --date='format-local:%Y%m%d_%H%M%S' --format="%cd")
-    fi
-else
-    DOCKER_TAG=local
-fi
-
 #In case we are in travis, we will use cached docker environment.
 if [[ -n "$TRAVIS" ]]; then
     DOCKER_COMMAND=container_cache
@@ -50,22 +37,25 @@ if ! [[ -n "$TRAVIS" ]]; then
 fi
 
 echoTitle "Building docker image for ${DOCKER_IMAGE}"
-echo "GIT BRANCH: ${GIT_BRANCH}"
-echo "GIT COMMIT: ${GIT_COMMIT}"
-echo "GIT COMMIT DATE: ${GIT_COMMIT_DATE}"
-echo "BUILD CREATOR: ${BUILD_CREATOR}"
+echo "GIT BRANCH: ${BRANCH}"
+echo "GIT COMMIT: ${COMMIT}"
+echo "GIT COMMIT DATE: ${COMMIT_DATE}"
+echo "BUILD CREATOR: ${CREATOR}"
+echo "BUILD DATE: ${CREATION_DATE}"
 echo "IMAGE NAME: ${DOCKER_IMAGE}:${DOCKER_TAG}"
 
 DOCKER_ARGS=" \
     -t ${DOCKER_IMAGE}:${DOCKER_TAG} \
-    --build-arg GIT_BRANCH="$GIT_BRANCH" \
-    --build-arg GIT_COMMIT="$GIT_COMMIT" \
-    --build-arg GIT_COMMIT_DATE="$GIT_COMMIT_DATE" \
-    --build-arg BUILD_CREATOR="$BUILD_CREATOR" \
-    --build-arg APPNAME="$APPNAME" \
-    --build-arg APPFOLDER="$APPFOLDER" \
-    -f docker/dockerfile \
-    ."
+	-f docker/dockerfile \
+	--build-arg APPNAME=${APPNAME} \
+	--build-arg GIT_COMMIT=${COMMIT} \
+	--label appname=${APPNAME} \
+	--label branch=${BRANCH} \
+	--label build-date=${CREATION_DATE} \
+	--label commit=${COMMIT} \
+	--label commit-author=${CREATOR} \
+	--label commit-date=${COMMIT_DATE} \
+	."
 
 echo "args: ${DOCKER_ARGS}"
 set -x
